bffa9d0cbb01f879d2f1c8119d8e2f3876b97d9d
rsgrinko <rsgrinko@gmail.com>
2023-05-16 17:54:59 +0300

    немного причесал код старой давности

--- custom_type_table.php
+++ custom_type_table.php
@@ -1,71 +1,104 @@
 <?php
-/*
-	Кастомный тип - Таблица
-	ООО SeoVen
-	Roman S Grinko <rsgrinko@gmail.com>
-*/
-AddEventHandler('iblock', 'OnIBlockPropertyBuildList', array('rsgrinko', 'GetUserTypeDescription'));
-class rsgrinko
-{
 
-    function GetUserTypeDescription()
-    {
-        return array(
-            'PROPERTY_TYPE' => 'S',
-            'USER_TYPE' => 'custom_table',
-            'DESCRIPTION' => 'Таблицы',
-            'GetPropertyFieldHtml' => array('rsgrinko', 'GetPropertyFieldHtml'),
-            'ConvertToDB' => array('rsgrinko', 'ConvertToDB'),
-            'ConvertFromDB' => array('rsgrinko', 'ConvertFromDB'),
-            'PrepareSettings' => array('rsgrinko', 'PrepareSettings'),
-            'GetSettingsHTML' => array('rsgrinko', 'GetSettingsHTML')            
-        );
-    }
-    
-    function PrepareSettings($arFields)
-    {
-        $col_count = intval($arFields['USER_TYPE_SETTINGS']['COL_COUNT']);
-        if($col_count <= 1) $col_count = 1;
-        $col_title = $arFields['USER_TYPE_SETTINGS']['COL_TITLE'];
-       
-        return array('COL_COUNT' => $col_count, 'COL_TITLE' => $col_title);
-    }
-	
-	function GetSettingsHTML($arProperty, $strHTMLControlName, &$arPropertyFields)
-    {
-        $arPropertyFields = array(
-            'HIDE' => array('FILTRABLE', 'COL_TITLE', 'COL_COUNT', 'DEFAULT_VALUE'),
-            'SET' => array('FILTRABLE' => 'N'),
-            'USER_TYPE_SETTINGS_TITLE' => 'Настройки кастомного свойства'
-        );
+    /**
+     * Реализация кастомных свойств для 1С-Битрикс
+     * Кастомный тип - Таблица
+     * Roman S Grinko <rsgrinko@gmail.com>
+     */
+    AddEventHandler('iblock', 'OnIBlockPropertyBuildList', ['rsgrinko', 'GetUserTypeDescription']);
 
-        return '<tr><td>Количество столбцов таблицы:</td><td><input type="text" size="50" name="'.$strHTMLControlName['NAME'].'[COL_COUNT]" value="'.$arProperty['USER_TYPE_SETTINGS']['COL_COUNT'].'"></td><td></td></tr>
-<tr><td>Названия столбцов через разделитель (<b>:||:</b>):</td><td><textarea name="'.$strHTMLControlName['NAME'].'[COL_TITLE]" cols="50" rows="3">'.$arProperty['USER_TYPE_SETTINGS']['COL_TITLE'].'</textarea></td></tr>';
-    }
-    
-    function GetPropertyFieldHtml($arProperty, $value, $strHTMLControlName)
+    class rsgrinko
     {
-	 	
-        $value = $value['VALUE'];
-        $field = '';
-        	$col_title = explode(':||:', $arProperty['USER_TYPE_SETTINGS']['COL_TITLE']);
-        	
-        	$field .= '<div style="display: flex" class="custom_property_title_'.$arProperty['CODE'].'">';
+
+        /**
+         * Получение описания
+         *
+         * @return array
+         */
+        function GetUserTypeDescription()
+        {
+            return [
+                'PROPERTY_TYPE'        => 'S',
+                'USER_TYPE'            => 'custom_table',
+                'DESCRIPTION'          => 'Таблицы',
+                'GetPropertyFieldHtml' => ['rsgrinko', 'GetPropertyFieldHtml'],
+                'ConvertToDB'          => ['rsgrinko', 'ConvertToDB'],
+                'ConvertFromDB'        => ['rsgrinko', 'ConvertFromDB'],
+                'PrepareSettings'      => ['rsgrinko', 'PrepareSettings'],
+                'GetSettingsHTML'      => ['rsgrinko', 'GetSettingsHTML'],
+            ];
+        }
+
+        /**
+         * Подготовка настроек
+         *
+         * @param $arFields
+         *
+         * @return array
+         */
+        function PrepareSettings($arFields)
+        {
+            $col_count = intval($arFields['USER_TYPE_SETTINGS']['COL_COUNT']);
+            if ($col_count <= 1) {
+                $col_count = 1;
+            }
+            $col_title = $arFields['USER_TYPE_SETTINGS']['COL_TITLE'];
+
+            return ['COL_COUNT' => $col_count, 'COL_TITLE' => $col_title];
+        }
+
+        /**
+         * Получение HTML настроек
+         *
+         * @param $arProperty
+         * @param $strHTMLControlName
+         * @param $arPropertyFields
+         *
+         * @return string
+         */
+        function GetSettingsHTML($arProperty, $strHTMLControlName, &$arPropertyFields)
+        {
+            $arPropertyFields = [
+                'HIDE'                     => ['FILTRABLE', 'COL_TITLE', 'COL_COUNT', 'DEFAULT_VALUE'],
+                'SET'                      => ['FILTRABLE' => 'N'],
+                'USER_TYPE_SETTINGS_TITLE' => 'Настройки кастомного свойства',
+            ];
+
+            return '<tr><td>Количество столбцов таблицы:</td><td><input type="text" size="50" name="' . $strHTMLControlName['NAME']
+                   . '[COL_COUNT]" value="' . $arProperty['USER_TYPE_SETTINGS']['COL_COUNT'] . '"></td><td></td></tr>
+<tr><td>Названия столбцов через разделитель (<b>:||:</b>):</td><td><textarea name="' . $strHTMLControlName['NAME']
+                   . '[COL_TITLE]" cols="50" rows="3">' . $arProperty['USER_TYPE_SETTINGS']['COL_TITLE'] . '</textarea></td></tr>';
+        }
+
+        /**
+         * Получение поля свойства в HTML
+         *
+         * @param $arProperty
+         * @param $value
+         * @param $strHTMLControlName
+         *
+         * @return string
+         */
+        function GetPropertyFieldHtml($arProperty, $value, $strHTMLControlName)
+        {
+            $value     = $value['VALUE'];
+            $field     = '';
+            $col_title = explode(':||:', $arProperty['USER_TYPE_SETTINGS']['COL_TITLE']);
+            $field .= '<div style="display: flex" class="custom_property_title_' . $arProperty['CODE'] . '">';
             for ($i = 0; $i < $arProperty['USER_TYPE_SETTINGS']['COL_COUNT']; $i++) {
-                $field .= '<span style="width: 100px;margin-right: 22px;">'.$col_title[$i].'</span>';
+                $field .= '<span style="width: 100px;margin-right: 22px;">' . $col_title[$i] . '</span>';
             }
             $field .= '</div>';
-        
             $field .= '<div style="display: flex">';
             for ($i = 0; $i < $arProperty['USER_TYPE_SETTINGS']['COL_COUNT']; $i++) {
-                $field .= '<input style="width: 100px; margin-right: 10px !important;" type="text" name="' . $strHTMLControlName["VALUE"] . '['.$i.']" value="' . $value[$i] . '">';
+                $field .= '<input style="width: 100px; margin-right: 10px !important;" type="text" name="' . $strHTMLControlName["VALUE"] . '[' . $i
+                          . ']" value="' . $value[$i] . '">';
             }
             $field .= '</div>';
             $field .= '<hr>';
-        
-			$field .= '<style>.custom_property_title_'.$arProperty['CODE'].'.not_show {display: none !important;}</style>
+            $field .= '<style>.custom_property_title_' . $arProperty['CODE'] . '.not_show {display: none !important;}</style>
 			<script>
-			Array.prototype.forEach.call(document.querySelectorAll(".custom_property_title_'.$arProperty['CODE'].'"), function(e, index){
+			Array.prototype.forEach.call(document.querySelectorAll(".custom_property_title_' . $arProperty['CODE'] . '"), function(e, index){
 			if(index==0){
 				e.classList.remove(\'not_show\');
 			} else {
@@ -73,33 +106,46 @@ class rsgrinko
 			}
 			});
 			</script>';
-        return $field;
-    }
-    
+            return $field;
+        }
 
-    function ConvertToDB($arProperty, $value)
-    {
-	      $return = false;
-        if(is_array($value) and array_key_exists('VALUE', $value))
+
+        /**
+         * Преобразование для сохранения в БД
+         *
+         * @param $arProperty
+         * @param $value
+         *
+         * @return array|false
+         */
+        function ConvertToDB($arProperty, $value)
         {
-            $arValue = array();
-            foreach($value['VALUE'] as $key => $value){
-               $arValue[$key] = htmlspecialchars($value);
+            $return = false;
+            if (is_array($value) and array_key_exists('VALUE', $value)) {
+                $arValue = [];
+                foreach ($value['VALUE'] as $key => $value) {
+                    $arValue[$key] = htmlspecialchars($value);
+                }
+                $return = ['VALUE' => serialize($arValue),];
             }
-            $return = array('VALUE' => serialize($arValue),);
-        }
-        
-        return $return;
-    }
 
-    function ConvertFromDB($arProperty, $value)
-    {
-        $return = false;
-        if(!is_array($value['VALUE']))
+            return $return;
+        }
+
+        /**
+         * Преобразование после извлечения из БД
+         * @param $arProperty
+         * @param $value
+         *
+         * @return array|false
+         */
+        function ConvertFromDB($arProperty, $value)
         {
-            $return = array('VALUE' => unserialize($value['VALUE']));
+            $return = false;
+            if (!is_array($value['VALUE'])) {
+                $return = ['VALUE' => unserialize($value['VALUE'])];
+            }
+            return $return;
         }
-        return $return;
-    }
 
-}
+    }
--- init.php
+++ init.php
@@ -1,4 +1,5 @@
 <?php
-if(file_exists($_SERVER['DOCUMENT_ROOT'].'/local/php_interface/custom_type_table.php')) {
-	require_once $_SERVER['DOCUMENT_ROOT'].'/local/php_interface/custom_type_table.php';
-}
+    // Подключаем обработку
+    if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/custom_type_table.php')) {
+        require_once $_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/custom_type_table.php';
+    }
